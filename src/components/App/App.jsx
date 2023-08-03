079c585260882d1ff4f5118b5c00ab61bba03183
Sergey Kiselev <sk.mails@yandex.ru>
2023-08-04 00:32:29 +0300

    fit: Собрана промо-страница

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,29 +1,11 @@
 import Header from '../Header/Header';
-import Promo from '../Main/Promo/Promo';
-import AboutProject from '../Main/AboutProject/AboutProject';
-import Techs from '../Main/Techs/Techs';
-import AboutMe from '../Main/AboutMe/AboutMe';
 import Footer from  '../Footer/Footer'
+import Main from '../Main/Main';
 
 function App() {
   return <>
     <Header/>
-    <Promo/>
-    <AboutProject
-      coreClass="aboutProject"
-      sectionHeader="О проекте"
-      addSectionClass="mainSection_content_aboutProject"
-    />
-    <Techs
-      coreClass="techs"
-      sectionHeader="Технологии"
-      addSectionClass="mainSection_content_techs"
-    />
-    <AboutMe
-      coreClass="aboutMe"
-      sectionHeader="Студент"
-      addSectionClass="mainSection_content_aboutMe"
-    />
+    {/*<Main/>*/}
 
     <Footer/>
   </>;
